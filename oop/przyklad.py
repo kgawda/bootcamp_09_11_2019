94b188b0e26b9fed0a16ea4cbe4c99127bbbe731
Konrad Gawda <k.gawda@alx.pl>
2019-12-21 12:41:30 +0100

    Początek OOP

---
+++ oop/przyklad.py
@@ -0,0 +1,21 @@
+class SwietyMikolaj:
+    def __init__(self, imie="Mikołaj"):
+        self.imie = imie
+
+    def przywitaj_sie(self):
+        print(f"Hohoho! Jestem {self.imie}")
+
+    def daj_prezent(self, dla_kogo):
+        return f"Prezent dla {dla_kogo}"
+
+mikolaj1 = SwietyMikolaj("Mikołajek")
+
+print("Ten SwietyMikolaj nazywa się", mikolaj1.imie)
+
+inni_mikolajowie = [SwietyMikolaj("Andrzej"), SwietyMikolaj("Bltazar"), SwietyMikolaj()]
+
+mikolaj1.przywitaj_sie()
+for mikolaj in inni_mikolajowie:
+    mikolaj.przywitaj_sie()
+
+print(mikolaj1.daj_prezent("Agaty"))

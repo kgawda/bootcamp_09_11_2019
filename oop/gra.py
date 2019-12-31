5a10a37dfe7f76494bccda6d9d4a26ec6e4bfc51
Konrad Gawda <k.gawda@alx.pl>
2019-12-31 18:10:34 +0100

    Modyfikacje gry

--- oop/gra.py
+++ oop/gra.py
@@ -5,9 +5,12 @@ class Pionek:
         self.x = 0
         self.y = 0
 
-    #TODO def umiesc_w_losowym_miejscu
+    def umiesc_w_losowym_miejscu(self, plansza_wymiar_x, plansza_wymiar_y):
+        self.x = random.randrange(0, plansza_wymiar_x)
+        self.y = random.randrange(0, plansza_wymiar_y)
 
     def przesun(self, kierunek):
+        # TODO: nie pozwolić na wyjście poza planszę (trzeba pojakoś poznać wymiary planszy)
         if kierunek == 'w':
             self.y += 1
         elif kierunek == 's':
@@ -28,12 +31,28 @@ class Boss(Wojownik):
         super().__init__(imie)
         self.punkty_zycia = punkty_zycia
 
-pionki = [Wojownik("Janusz"), Wojownik("Grażyna"), Wojownik("Brajan"), Boss("Seba")]
-for pionek in pionki:
-    print(f"{pionek.imie}:\t\t{pionek.x}, {pionek.y}")
-print()
-for _ in range(10):
+def plansza_jako_string(pionki, plansza_wymiar_x, plansza_wymiar_y):
+    #s = ""
+    #for x in range(plansza_wymiar_y):
+    #    for y in range(plansza_wymiar_x):
+    #        pass
+    #        # TODO: jeśli pod tymi współrzędnymi jest pionek, dopisz do s 
+    #        #  literę, która go reprezentuje. Jeśli nie ma - dopisz kropkę.
+    #        #  Zakładamy, że w tym samym miejscu jest tylko jeden pionek.
+    #        #  Uwaga: może być potrzebna pętla for
+    #    s += "\n"
+    #return s
+    
+    s = ""
     for pionek in pionki:
-        pionek.przesun(random.choice("wsad"))
-for pionek in pionki:
-    print(f"{pionek.imie}:\t\t{pionek.x}, {pionek.y}")
+        s += f"{pionek.imie}:\t\t{pionek.x}, {pionek.y}\n"
+    return s
+
+if __name__ == "__main__":
+    pionki = [Wojownik("Janusz"), Wojownik("Grażyna"), Wojownik("Brajan"), Boss("Seba")]
+    print(plansza_jako_string(pionki, 20, 20))
+    print()
+    for pionek in pionki:
+        pionek.umiesc_w_losowym_miejscu(20, 20)
+    print(plansza_jako_string(pionki, 20, 20))
+
